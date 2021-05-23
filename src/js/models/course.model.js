83bf0cfa9801d831332517e698a0543e40042251
ehab qino <ehabqino@gmail.com>
2021-05-23 17:57:27 +0300

    commit

--- src/js/models/course.model.js
+++ src/js/models/course.model.js
@@ -2,7 +2,7 @@ define(['ojs/ojModel'],
     function() {
         class CourseModel{
             constructor(){
-
+                this.serverUrl= "http://localhost:2480/";
             }//
 
             initializeModelCollection(endpoint){
@@ -18,6 +18,28 @@ define(['ojs/ojModel'],
                 this.courses = new this.courseCollDef;
             }//
             getCoursesMenu(notify){
+                let api_url = this.serverUrl+"query/ehabcourses/sql/SELECT FROM course";
+                this.initializeModelCollection(api_url);
+                let courseRow = new this.courseModelDef({},this.courses);
+                courseRow.fetch({
+                    success:(coll,data)=>{
+                        console.log("Success");
+                        console.log(data);
+                    },//
+                    error:(model,xhr,options)=>{
+                        console.log("Error");
+                        console.log(options);
+                    },//
+                    Headers:{
+                        'Authorization' : 'Basic cm9vdDpyb290cHdk',
+                        'Content-Type' : 'application/json'
+                        
+                        //if i want to manualy encrypted the authorization using basic 64 encryption
+                        //'Authorization' : 'Basic'+btoa('root':'rootpwd')
+                    }//Headers
+                });//end fetch
+
+
                 let navData = [
                     { path: '', redirect: 'dashboard' },
                     { path: 'dashboard', detail: { label: 'Dashboard', iconClass: 'oj-ux-ico-bar-chart' } },
