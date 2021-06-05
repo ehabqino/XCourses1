9e93377059c8c93c48b5d93d0db96165fdb5e3b3
ehab qino <ehabqino@gmail.com>
2021-06-05 15:53:20 +0300

    commit

--- src/js/utils/messageBroker.js
+++ src/js/utils/messageBroker.js
@@ -1,6 +1,21 @@
 define([], 
     function() {
         class MsgBroker {
+            constructor(){
+                this.subcribers = {}
+            }
+            publish(groupName, data){
+                if(! Array.isArray(this.subcribers[groupName]))
+                    return;
+                this.subcribers[groupName].forEach(callbackNotify => {
+                    callbackNotify(data);
+                });
+
+            }//end publish
+
+            subcribe(groupName, callbackNotify){
+
+            }//end subcribe
 
         }// end MsgBroker class
         return new MsgBroker;
